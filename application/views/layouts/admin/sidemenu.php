f37edee897eb85009e91248c3f736ed18f14a0c6
Mahesh <mtailor@keypoint-tech.com>
2021-09-06 16:14:10 +0530

    ifas list based on hod userId api build

--- application/views/layouts/admin/sidemenu.php
+++ application/views/layouts/admin/sidemenu.php
@@ -34,8 +34,8 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+          <li class="nav-item">
+            <a href="#" class="nav-link ">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -53,7 +53,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="<?php echo base_url('registration'); ?>" class="nav-link">
+                <a href="<?php echo base_url('registration'); ?>" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Create IFA User</p>
                 </a>
